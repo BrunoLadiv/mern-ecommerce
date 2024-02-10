1c283db8e82e6bcb06a913d56d14d366b2d2e0f7
Bruno <brunokasprik@gmail.com>
2024-02-10 04:34:23 -0300

    add rating component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { Container } from 'react-bootstrap'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import './App.css'
 
 function App() {
   return (
