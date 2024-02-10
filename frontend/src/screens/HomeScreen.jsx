ef3f05959086d582c9efe9e86897bc8b71912bdf
Bruno <brunokasprik@gmail.com>
2024-02-10 06:47:21 -0300

    add axios and made the front end get products from backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,19 +1,29 @@
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
-
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 export default function HomeScreen() {
+  const [products, setProducts] = useState([])
+//   console.log(products)
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => {
-          return (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          )
-        })}
+        {products &&
+          products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            )
+          })}
       </Row>
     </>
   )
