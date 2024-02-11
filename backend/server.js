c4862ee050205fc981d203b63c4cad78f6b0742f
Bruno <brunokasprik@gmail.com>
2024-02-11 04:28:32 -0300

    add error handler

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import products from './data/products.js'
 import productRoutes from './routes/productRoutes.js' 
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorHandler.js'
 
 dotenv.config()
 import connectDB from './config/db.js'
@@ -15,6 +16,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`app listening on port ${port || 3000}!`)
