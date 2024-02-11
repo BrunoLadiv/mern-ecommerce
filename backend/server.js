6a334b4cc017270ab5fd970f1889f308934ed01f
Bruno <brunokasprik@gmail.com>
2024-02-11 04:14:29 -0300

    add products route

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js' 
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -13,15 +14,7 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 app.listen(port, () => {
   console.log(`app listening on port ${port || 3000}!`)
