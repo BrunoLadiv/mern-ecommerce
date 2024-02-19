36147d5a78cdc4ed6c18b6f5e5056dd4c16fb327
Bruno <brunokasprik@gmail.com>
2024-02-19 03:44:13 -0300

    add helper function to generate tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js'
-import jwt from 'jsonwebtoken'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc Auth user
 // @route GET /api/users/login
@@ -10,15 +10,7 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
   const user = await User.findOne({ email })
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    })
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-    })
+    generateToken(res, user._id)
     res.json({
       _id: user._id,
       name: user.name,
@@ -45,6 +37,23 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error('User already exists')
   }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+  if (user) {
+    generateToken(res, user._id)
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // @desc Logout user / clear cookie
@@ -52,12 +61,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Private
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.cookie('token','',{
+  res.cookie('token', '', {
     httpOnly: true,
-    expires: new Date(0)
-
+    expires: new Date(0),
   })
-  res.status(200).json({success: true, message: 'User logged out'})
+  res.status(200).json({ success: true, message: 'User logged out' })
 })
 
 // @desc Get user profile
