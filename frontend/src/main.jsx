0cf33022773857ce2afece958997152d0b8779e0
Bruno <brunokasprik@gmail.com>
2024-02-22 06:06:17 -0300

    feat register feature working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,6 +16,7 @@ import {
 } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen.jsx'
 import LoginScreen from './screens/LoginScreen.jsx'
+import RegisterScreen from './screens/RegisterScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,6 +25,7 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen/>} />
       <Route path="/login" element={<LoginScreen/>} />
+      <Route path="/register" element={<RegisterScreen/>} />
 
     </Route>
   )
