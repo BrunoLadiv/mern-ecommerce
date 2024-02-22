fd0168a940e4c734b65f33b3e936805f4bcbe750
Bruno <brunokasprik@gmail.com>
2024-02-22 09:17:35 -0300

    add order page, order routes/controllers

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express, { application } from 'express'
 import products from './data/products.js'
 import productRoutes from './routes/productRoutes.js' 
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import { notFound, errorHandler } from './middleware/errorHandler.js'
@@ -22,6 +23,7 @@ app.get('/', (req, res) => {
 })
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
