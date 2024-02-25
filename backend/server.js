27bf5e31f55d7d2b1182ee26c58d5b58927ccedd
Bruno <brunokasprik@gmail.com>
2024-02-25 07:24:20 -0300

    add uploadroutes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express, { application } from 'express'
 import products from './data/products.js'
 import productRoutes from './routes/productRoutes.js'
@@ -6,6 +7,7 @@ import orderRoutes from './routes/orderRoutes.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import { notFound, errorHandler } from './middleware/errorHandler.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 import connectDB from './config/db.js'
@@ -24,9 +26,12 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use ('/api/upload', uploadRoutes)
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads') ))
 
 app.use(notFound)
 app.use(errorHandler)
