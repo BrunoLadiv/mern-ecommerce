3642563fb93f2e3144101798db7b9fc30a905948
Bruno <brunokasprik@gmail.com>
2024-02-25 07:46:38 -0300

    add admin useredit screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -29,6 +29,7 @@ import OrderListScreen from './screens/admin/OrderListSreen.jsx'
 import ProductListScreen from './screens/admin/ProductListScreen.jsx'
 import ProductEditScreen from './screens/admin/ProductEditScreen.jsx'
 import UserListScreen from './screens/admin/UserListScreen.jsx'
+import UserEditScreen from './screens/admin/UserEditScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -50,6 +51,7 @@ const router = createBrowserRouter(
         <Route path="/admin/userlist" element={<UserListScreen />} />
         <Route path="/admin/orderlist" element={<OrderListScreen />} />
         <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
+        <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
       </Route>
     </Route>
   )
